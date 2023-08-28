cab57fcc415001a32fbc0b717f0fa4efecb9cb6e
Oscar McGoldrick <oscar.mcgoldrick99@gmail.com>
2023-08-28 16:24:42 +1200

    boilerplate in place db semi started

---
+++ server/db/migrations/20230828035610_users.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id').primary()
+    table.timestamp('created_at').defaultTo(Date.now)
+    table.string('f_name')
+    table.string('l_name')
+    table.string('email')
+    table.string('auth0_id')
+    table.string('refresh_token')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+};
